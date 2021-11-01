5f6358715fb3f2df939f16d25fcda69191d8e9bf
Oleg Ivanov <o.ivanov@g.nsu.ru>
2021-11-01 11:29:24 +0700

    tests created

--- lab1_3_2/src/main/java/studentBook.java
+++ lab1_3_2/src/main/java/studentBook.java
@@ -1,139 +1,147 @@
+import java.util.Arrays;
+
 public class studentBook {
-    public class gradeExeption extends Exception{
-        public gradeExeption(String a){
+    public class gradeExeption extends Exception {
+        public gradeExeption(String a) {
             super(a);
         }
     }
-    private class lesson{
-        void set(String name,int f,int l)
-        {
-            nam=name;
-            fsem=f;
-            lsem=l;
+
+    private class lesson {
+        lesson(String name, int f, int l) {
+            nam = name;
+            fsem = f;
+            lsem = l;
         }
+
+        int[] grades;
         String nam;
         int fsem;
         int lsem;
     }
+
+    void extArr(int ln) {
+        classes = Arrays.copyOf(classes, ln);
+    }
+
     private String name;
-    private int[][] grades;
-    private lesson[] classes;
-    private int n;
-    private int i=0;
+    private lesson[] classes=new lesson[1];
+    private int n = 0;
+    private int cap = 1;
 
-    private int semesterNum=0;
+    private int semesterNum = 1;
+    private int semestrStip=0;
 
-    private int medGrade=0;
-    private int medGradecnt=0;
+    private int medGrade = 0;
+    private int medGradecnt = 0;
 
-    private int diplomflag=0;
-    private int diplomGrade=0;
-    private int diplomGradecnt=0;
+    private int diplomflag = 0;
+    private int diplomGrade = 0;
+    private int diplomGradecnt = 0;
 
-    private int qualGrade=0;
+    private int qualGrade = 0;
 
     /**
-     *
      * @param val -name of a student
-     * @param num -number of classes
      */
-    public studentBook(String val,int num){
-        n=num;
+    public studentBook(String val) {
         String name = val;
-        grades=new int[n][4*n];
-        classes=new lesson[n];
     }
+
     public void setqGrade(int grade) throws Exception {
-        if(grade>5||grade<2) {
-            Exception e=new gradeExeption("qualification grade-Has invalid grade. Grade must be between 2 and 5");
+        if (grade > 5 || grade < 2) {
+            Exception e = new gradeExeption("qualification grade-Has invalid grade. Grade must be between 2 and 5");
             throw e;
         }
-        qualGrade=grade;
+        qualGrade = grade;
     }
+
     /**
-     *
-     * @param nam - name of a class
+     * @param nam      - name of a class
      * @param semester - number of a semester
      * @param newgrade - new grade
      * @throws Exception
      */
-    public void changeGrade(String nam,int semester,int newgrade) throws Exception {
+    public void setGrade(String nam, int semester, int newgrade) throws Exception {
         int j;
-        for(j=0;j<n;j++){if(nam!=classes[j].nam) break;}
-        if(j==n){
-            Exception e=new gradeExeption(classes[j].nam + "- No such class found");
+        for (j = 0; j < n; j++) {
+            if (nam == classes[j].nam) break;
+        }
+        if (j == n) {
+            Exception e = new gradeExeption(nam + "- No such class found");
             throw e;
         }
-        if(semester>classes[j].fsem || semester<classes[j].lsem){
-            Exception e=new gradeExeption(classes[j].nam + "- There is no such class in this semester");
+        if (semester > classes[j].lsem || semester < classes[j].fsem) {
+            Exception e = new gradeExeption(classes[j].nam + "- There is no such class in this semester or invalid semester number");
             throw e;
         }
-        if(newgrade>5 || newgrade<2){
-            Exception e=new gradeExeption(classes[j].nam + "-Has invalid grade. Grade must be between 2 and 5");
+        if (newgrade > 5 || newgrade < 2) {
+            Exception e = new gradeExeption(classes[j].nam + "-Has invalid grade. Grade must be between 2 and 5");
             throw e;
         }
-        else{
-            if(grades[j][semester]<4&&newgrade>3){
+        int sem=(semester-1)%3;
+        if (classes[j].grades[sem] != 0) {
+            if (classes[j].grades[sem] < 4 && newgrade > 3) {
                 diplomflag--;
             }
-            grades[j][semester]=newgrade;
+            if (classes[j].grades[sem] > 3 && newgrade < 4) {
+                diplomflag++;
+            }
+            medGrade = medGrade - classes[j].grades[sem] + newgrade;
+            if (semester == classes[j].fsem) {
+                diplomGrade = diplomGrade - classes[j].grades[sem] + newgrade;
+            }
+        } else {
+            if (newgrade< 4){
+                diplomflag++;
+            }
+            medGrade+=newgrade;
+            medGradecnt++;
+            if (semester == classes[j].lsem) {
+                diplomGrade+=newgrade;
+                diplomGradecnt++;
+            }
         }
+        if (semester == semesterNum+1) {
+            semesterNum = semester;
+        }
+        classes[j].grades[sem] = newgrade;
+
     }
+
     /**
-     *
-     * @param val -name of a class
+     * @param val            -name of a class
      * @param first_semester - number of first semester
-     * @param last_semester - number of last semester
+     * @param last_semester  - number of last semester
      * @throws Exception
      */
-    public void addClass(String val,int first_semester,int last_semester) throws Exception {
-        if (first_semester<1 || first_semester>last_semester || last_semester<1){
-            Exception e = new gradeExeption(classes[i].nam + " Has invalid number of semesters");
+    public void addClass(String val, int first_semester, int last_semester) throws Exception {
+        if (first_semester < 1 || first_semester > last_semester || last_semester < 1 || last_semester>10) {
+            Exception e = new gradeExeption(val + " Has invalid number of semesters");
             throw e;
         }
-        classes[i++].set(val,first_semester,last_semester);
+        if (n >= cap)
+            extArr(cap = n * 2 + 1);
+        classes[n]=new lesson (val, first_semester, last_semester);
+        classes[n].grades = new int[last_semester - first_semester + 1];
+        n++;
     }
 
-    /**
-     *
-     * @param semesterGrades - semester grades
-     * @throws Exception
-     */
-    public void addGrades(int[] semesterGrades) throws Exception {
-        if (semesterGrades.length!=n){
-            Exception e=new gradeExeption("There must be as much grades as classes");
-            throw e;
-        }
-       for(int j=0;j<n;j++){
-           if(semesterGrades[j]>5 || semesterGrades[j]<2){
-                Exception e=new gradeExeption(classes[j].nam + "-Has invalid grade. Grade must be between 2 and 5");
-                throw e;
-           }
-           grades[j][semesterNum]=semesterGrades[j];
-           boolean x =classes[j].lsem<=semesterNum&&classes[j].fsem>=semesterNum;
-           medGrade+=x?semesterGrades[j]:0;
-           medGradecnt+=x?1:0;
-           if (semesterGrades[j]<4&&x) {
-               diplomflag++;
-           }
-           x=classes[j].fsem==semesterNum;
+    public double mediumGrades() {
+        if(medGradecnt==0)return 0;
+        return ((double) medGrade / (double) medGradecnt);
+    }
 
-           diplomGrade+=x?semesterGrades[j]:0;
-           diplomGradecnt+=x?1:0;
-       }
-        semesterNum++;
+    public boolean diplom() {
+        if(diplomGradecnt==0)return false;
+        return ((double) diplomGrade / (double) diplomGradecnt) >= 4.75 && (diplomflag == 0) && qualGrade == 5;
     }
-    public double mediumGrades(){
-        return ((double)medGrade/(double)medGradecnt);
-    }
-    public boolean diplom(){
-        return ((double)diplomGrade/(double)diplomGradecnt)>=4.75&&(diplomflag==0)&&qualGrade==5;
-    }
-    public boolean stipendia(){
-        for (int j=0;j<n;j++){
-        boolean x =classes[j].lsem<=semesterNum&&classes[j].fsem>=semesterNum;
-        if (grades[j][semesterNum]!=5&&x)
-            return false;
+
+    public boolean stipendia() {
+        for (int j = 0; j < n; j++) {
+            boolean x = classes[j].lsem >= semesterNum && classes[j].fsem <= semesterNum;
+            if (classes[j].grades[(semesterNum-1)%3] != 5 && x)
+                return false;
         }
         return true;
 
--- lab1_3_2/src/test/java/test.java
+++ lab1_3_2/src/test/java/test.java
@@ -1,32 +1,51 @@
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.testng.Assert.assertEquals;
 
 public class test {
 
-    studentBook a = new studentBook("Ivan",4);
+   private studentBook init() throws Exception {
+       studentBook a = new studentBook("Ivan");
+       a.addClass("math",1,3);
+       a.addClass("OS",1,1);
+       a.addClass("TV",2,3);
+       return a;
+   }
     @Test
-    public void diplomtest() throws Exception {
-        a.addClass("Math",1,2);
-        a.addClass("OS",1,3);
-        a.addClass("Prolog",2,3);
-        a.addClass("II",3,3);
-        a.addGrades(new int[]{3, 5, 5, 5});
-        a.addGrades(new int[]{5,5,5,5});
+    public void Functest() throws Exception {
+        studentBook a = init();
         a.setqGrade(5);
-        a.addGrades(new int[]{5, 5, 5, 5});
-        assert (!a.diplom());
-        a.changeGrade("Math",1,5);
-        assert (a.diplom());
-        a.setqGrade(4);
-        assert (!a.diplom());
-    }
-    @Test
-    public void medGradeTest() throws Exception{
+        a.setGrade("math",1,5);
+        a.setGrade("OS",1,5);
+        assert (a.stipendia());
         assert(a.mediumGrades()==5);
+        assert (a.diplom());
+        a.setGrade("TV",2,5);
+        a.setGrade("math",2,4);
+        assert(!a.stipendia());
     }
     @Test
-    public void invalidGradeTest(){
-
+    public void invalidGradeTest() throws Exception {
+        studentBook a = init();
+        studentBook.gradeExeption e = assertThrows(studentBook.gradeExeption.class, () ->
+        {a.setGrade("x",1,2);});
+        assertEquals(e.getMessage(),"x- No such class found");
+        studentBook.gradeExeption e1 = assertThrows(studentBook.gradeExeption.class, () ->
+        {a.setGrade("math",-1,2);});
+        assertEquals(e1.getMessage(),"math- There is no such class in this semester or invalid semester number");
+        studentBook.gradeExeption e2 = assertThrows(studentBook.gradeExeption.class, () ->
+        {a.setGrade("math",1,7);});
+        assertEquals(e2.getMessage(),"math-Has invalid grade. Grade must be between 2 and 5");
+        studentBook.gradeExeption e3 = assertThrows(studentBook.gradeExeption.class, () ->
+        {a.setqGrade(7);});
+        assertEquals(e3.getMessage(),"qualification grade-Has invalid grade. Grade must be between 2 and 5");
+    }
+    @Test
+    public void invalidClassTest() throws Exception {
+        studentBook a = init();
+        studentBook.gradeExeption e2 = assertThrows(studentBook.gradeExeption.class, () ->
+        {a.addClass("new",-1,7);});
+        assertEquals(e2.getMessage(),"new Has invalid number of semesters");
     }
 }
