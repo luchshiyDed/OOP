3cf4db09370c7b74d900b753af291718ad716da0
Oleg Ivanov <o.ivanov@g.nsu.ru>
2021-11-22 16:21:52 +0700

    yea

---
+++ lab1_4_2/src/main/java/main.java
@@ -0,0 +1,43 @@
+import notebook.Notebook;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+public class main {
+    public static void main(String args[]) {
+        int ln = args.length;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
+        Notebook a = new Notebook(args[0]+".json");
+        try {
+            switch (args[1]) {
+                case ("-add")-> {
+                    String str = ln >= 3 ? args[3] : "";
+                    a.addNote(args[2], str);
+                }
+                case ("-rm")-> {
+                    a.removeNote(args[2]);
+                }
+                case ("-show")->{
+                    if (ln == 2)
+                        a.printBook();
+                    else {
+                        try {
+                            String str[]=ln>3?Arrays.copyOfRange(args,4,ln): new String[]{""};
+                            a.printBook(dateFormat.parse(args[2]),dateFormat.parse(args[3]),str);
+                        } catch (ParseException e) {
+                            System.err.println("Invalid date formate please enter the date:yyyy-MM-dd kk:mm:ss");;
+                        }
+                    }
+                }
+                default -> {
+                    return;
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("not enough arguments for " + args[1] + " " + e.getMessage());
+        }
+    }
+
+}
---
+++ lab1_4_2/src/main/java/notebook/Notebook.java
@@ -0,0 +1,162 @@
+package notebook;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class Notebook {
+    private String df="yyyy-MM-dd kk:mm:ss";
+    private class Note implements Comparable {
+        private String title;
+        private String text;
+        private Date date;
+
+        Note(String title, String text) {
+            this.title = title;
+            this.text = text;
+            this.date = new Date();
+        }
+
+        private Note() {
+        }
+
+        public Date noteDate() {
+            return date;
+        }
+
+        public String noteText() {
+            return text;
+        }
+
+        public String noteTitle() {
+            return title;
+        }
+
+        public void changeTime(Date time) {
+            this.date = time;
+        }
+
+        public void changeTitle(String title) {
+            this.title = title;
+        }
+
+        public void changeNote(String note) {
+            this.text = note;
+        }
+
+
+        @Override
+        public int compareTo(Object o) {
+            Note a = (Note) o;
+            return (noteDate().compareTo(a.noteDate()));
+        }
+    }
+
+    private Gson gson = new Gson();
+    private String name;
+
+    public Notebook(String str) {
+        this.name = str;
+    }
+
+    private ArrayList<Note> readNotes() {
+        try {
+            Reader r = new FileReader(name);
+            Note[] notearr = gson.fromJson(r, Note[].class);
+            ArrayList<Note> notelist = new ArrayList<>(Arrays.asList(notearr));
+            return notelist;
+        } catch (FileNotFoundException e) {
+            return new ArrayList<Note>();
+        }
+    }
+
+    private void writeNotes(List<Note> val) {
+        try (Writer w = new FileWriter(name)) {
+            gson.toJson(val, w);
+        } catch (IOException e) {
+            System.err.println("Notes weren't saved:" + e.getMessage());
+            ;
+        }
+    }
+
+    public void addNote(String tit, String txt) {
+        Note note = new Note(tit, txt);
+        List<Note> book = readNotes();
+        book.add(note);
+        writeNotes(book);
+    }
+
+    public void removeNote(String title) {
+        List<Note> notelist = readNotes();
+
+        if (notelist.removeIf(Note -> Note.noteTitle().equals(title)))
+            writeNotes(notelist);
+        else
+            System.err.println("Failed to delete note with title:" + title);
+    }
+
+    private void printNote(Note note, DateFormat a) {
+
+        System.out.println(a.format(note.noteDate()));
+        System.out.println(note.noteTitle());
+        System.out.println(note.noteText());
+    }
+
+    public void printBook() {
+        Reader r = null;
+        try {
+            r = new FileReader(name);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
+        Note[] notearr = gson.fromJson(r, Note[].class);
+        Arrays.sort(notearr);
+        DateFormat dateFormat = new SimpleDateFormat(df);
+        for (Note note : notearr) printNote(note, dateFormat);
+    }
+
+    public void printBook(Date beg, Date end, String[] keyWs) {
+        DateFormat dateFormat = new SimpleDateFormat(df);
+        Reader r = null;
+        try {
+            r = new FileReader(name);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
+        Note[] notearr = gson.fromJson(r, Note[].class);
+        Arrays.sort(notearr);
+        int i = 0;
+        int b, e;
+
+        try{
+            for (i = 0; notearr[i].noteDate().compareTo(beg)<=0; i++) ;
+        }catch(IndexOutOfBoundsException a){
+            i=notearr.length;
+        }
+        b = i; if (i== notearr.length) return;
+        try{
+            for (i = i; notearr[i].noteDate().compareTo(end)<=0; i++) ;
+        }catch(IndexOutOfBoundsException a){
+            i=notearr.length;
+        }
+
+        e = i;
+        notearr = Arrays.copyOfRange(notearr, b, e);
+        for (Note note : notearr) {
+            boolean x = keyWs.length==0;
+            for (String str : keyWs) {
+                x = note.noteTitle().contains(str)||str=="";
+            }
+
+            if (x)
+                printNote(note, dateFormat);
+        }
+
+    }
+}
